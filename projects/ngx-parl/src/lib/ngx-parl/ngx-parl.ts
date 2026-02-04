b76020b25fadc73bfa70c11e04bd533435ced34d
cattallina <cattalina017@gmail.com>
2026-02-04 14:25:12 +0000

    fix: fixed code

--- projects/ngx-parl/src/lib/ngx-parl/ngx-parl.ts
+++ projects/ngx-parl/src/lib/ngx-parl/ngx-parl.ts
@@ -90,7 +90,6 @@ export class NgxParlComponent {
                 return [...list, updatedMessage];
             });
 
-            this.scrollToBottomTrigger.update(v => v + 1);
             this.scrollToBottom();
         });
     }
